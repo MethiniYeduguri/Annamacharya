3eab493a3beb3d787d671783cdca7667af7dc431
MethiniYeduguri <168398953+MethiniYeduguri@users.noreply.github.com>
2025-06-19 13:30:23 +0530

    Create SortTheLsitAlphabatically.java

---
+++ CRT_Programs/SortTheLsitAlphabatically.java
@@ -0,0 +1,18 @@
+
+package xyz;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortTheLsitAlphabatically {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        Collections.sort(list);  
+
+        System.out.println(list);   
+}
+}
+
